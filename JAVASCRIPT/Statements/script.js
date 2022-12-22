d30f8de9afca014a79f1b36e1fb28714295052f1
Rafael <rafaell_mirandaa@outlook.com>
2022-12-22 14:11:34 -0300

    Statements

---
+++ JAVASCRIPT/Statements/script.js
@@ -0,0 +1,21 @@
+let a, b, c, d; // Declare 4 variables
+
+a = 6; // Assign the value 6 to a
+b = 3; // Assign the value 3 to b
+c = 2013; // Assign the value 2013 to c
+d = a + b + c; // Assign the sum of a, b and c to d
+
+document.getElementById('result').innerHTML = d;
+
+//White Space
+
+/* let person = "Maya";
+let person="Maya" */
+
+// Code Blocks
+
+function show() {
+    document.getElementById('phrase1').innerHTML = 'Hi there!';
+    document.getElementById('phrase2').innerHTML = 'How are you?';
+}
+
