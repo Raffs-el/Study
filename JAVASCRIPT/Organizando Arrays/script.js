d850e27c083274873474430e11ef5840e7983f2f
Rafael <rafaell_mirandaa@outlook.com>
2023-01-05 15:08:45 -0300

    Organizando Arrays

---
+++ JAVASCRIPT/Organizando Arrays/script.js
@@ -0,0 +1,5 @@
+const frutas = [" Limão", " Uva", " Melancia", " Banana"];
+document.getElementById('antes').innerHTML = frutas;
+
+frutas.sort();
+document.getElementById('depois').innerHTML = frutas;
